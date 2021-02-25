5ba6fb1e3e035b7d5a3e5a8a25f155d6fdaaee14
mitanko <mita3ta@ezweb.ne.jp>
2021-02-25 23:40:28 +0900

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,34 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    address: "",
+    zip: ""
   },
   mutations: {
+    getAddress(state, zip, address) {
+      state.zip = zip;
+      state.address = address;
+    }
   },
   actions: {
-  },
-  modules: {
+    async getAddressAction(context) {
+      const payload = {
+        address: "",
+        zip: context.state.zip
+      };
+      await axios
+        .get("https://api.zipaddress.net/?", {
+          params: { zipcode: payload.zip }
+        })
+        .then(res => {
+          payload.address = res.data.data.fullAddress;
+        });
+      context.commit("getAddress", payload);
+    }
   }
-})
+});
